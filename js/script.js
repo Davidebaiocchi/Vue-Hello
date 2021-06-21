3158210d3115b39b18a8def586f7f4f8bb08c5f6
davide <davide.baiocchi97@gmail.com>
2021-06-21 13:50:18 +0200

    end without bonus

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+var message = new Vue({
+    el: '#mex',
+    data: {
+      sms: 'Kobe Bryant, il più forte di tutti i tempi!'
+    }
+  })
